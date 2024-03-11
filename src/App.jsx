121eb8b7774c189295943c1320fae4ca6553afa4
LarsGJobloop <lars.gunnar@jobloop.no>
2024-03-11 12:35:34 +0100

    setup initial pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
+import { LandingPage } from './pages/LandingPage'
+import { AboutPage } from './pages/about/AboutPage'
+import { NotFoundPage } from './pages/NotFoundPage'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={ <LandingPage /> } />
+        <Route path='/about' element={ <AboutPage /> } />
+        <Route path='*' element={ <NotFoundPage /> } />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
