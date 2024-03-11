6c67765af72e09dcd8ae3455a8e4a1456bd178a4
LarsGJobloop <lars.gunnar@jobloop.no>
2024-03-11 13:04:45 +0100

    setup root layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,19 @@ import {
 import { LandingPage } from './pages/LandingPage'
 import { AboutPage } from './pages/about/AboutPage'
 import { NotFoundPage } from './pages/NotFoundPage'
+import { LayoutRoot } from './pages/LayoutRoot'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route path='/' element={ <LandingPage /> } />
-        <Route path='/about' element={ <AboutPage /> } />
-        <Route path='*' element={ <NotFoundPage /> } />
+        <Route path='/' element={<LayoutRoot />}>
+          {/* index signifies that this is the default route */}
+          <Route index element={ <LandingPage /> } />
+          <Route path='/about' element={ <AboutPage /> } />
+          <Route path='*' element={ <NotFoundPage /> } />
+        </Route>
 
       </Routes>
     </BrowserRouter>
---
+++ src/pages/LayoutRoot.jsx
@@ -0,0 +1,41 @@
+import { Outlet, NavLink } from "react-router-dom"
+
+export function LayoutRoot() {
+  return (
+    <div>
+      <header>
+        <p>Routing</p>
+
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({isActive}) => isActive ? "active" : ""}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({isActive}) => isActive ? "active" : ""}
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <div>
+        {/* All subpages will replace this Outlet component */}
+        <Outlet />
+      </div>
+
+      <footer>
+        <p>Module D</p>
+      </footer>
+    </div>
+  )
+}
